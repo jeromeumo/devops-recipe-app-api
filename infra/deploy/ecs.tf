a10686025d8ae3a4d3558dbcf6eea8a0b2c6fb4e
Jeromeumo <jeromeumo@gmail.com>
2025-03-27 07:55:50 +0000

    added ecs cluster

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -1,7 +1,7 @@
-##
-# ECS Cluster for running app on Fargate.
-##
+##########################################
+# ECS cluster for runnung app on Fargate #
+##########################################
 
 resource "aws_ecs_cluster" "main" {
-  name = "${local.prefix}-cluster"
+    name = "${local.prefix}-cluster"
 }
